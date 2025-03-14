9bd5e246399d2999ef06226558fb21503f9e8af9
Emane <EmmaLB@users.noreply.huggingface.co>
2025-03-14 16:14:12 +0000

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from langchain.chains import ConversationalRetrievalChain # type: ignore
 from htmlTemplate import css , bot_template , user_template
 import os
 
-os.system("pip install --upgrade faiss-cpu torch huggingface_hub==0.15.1 sentence-transformers==2.2.2 InstructorEmbedding")
+os.system("pip install --upgrade faiss-cpu torch huggingface_hub sentence-transformers==2.2.2 InstructorEmbedding")
 
 
 def get_pdf_text(pdf_docs):
